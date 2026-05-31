459622eb1a9993d5d672643ae68909a266e3a4ba
adhitymarto <adhitymarto@gmail.com>
2026-05-31 14:26:23 +0700

    Create lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'YOUR_SUPABASE_URL',
+    anonKey: 'YOUR_SUPABASE_ANON_KEY',
+  );
+
+  runApp(const MyApp());
+}
